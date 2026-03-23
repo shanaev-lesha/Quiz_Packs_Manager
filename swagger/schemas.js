2a87c125ac47c7563d2baddf1b3c3bc7204f16da
Shanaev-lesha <shanaev007@gmail.com>
2026-03-23 18:57:23 +0400

    Все endpoints из TASK 3.4 описаны, Описаны схемы: QuestionPack, CreateQuestionPackRequest, UpdateQuestionPackRequest, Указано требование JWT, Swagger UI отображает документацию без ошибок

--- swagger/schemas.js
+++ swagger/schemas.js
@@ -18,6 +18,28 @@ export const schemas = {
 
   Token: {
     token: "jwt.token.here"
-  }
+  },
 
+   QuestionPack: {
+    id: "uuid",
+    user_id: "uuid",
+    title: "Название пака",
+    description: "Описание",
+    status: "draft",
+    created_at: "2026-01-01T10:00:00Z",
+    updated_at: "2026-01-01T10:00:00Z"
+  },
+
+  CreateQuestionPackRequest: {
+    $title: "My pack",
+    description: "Описание",
+    status: "draft"
+  },
+
+  UpdateQuestionPackRequest: {
+    title: "Updated title",
+    description: "Новое описание",
+    status: "published"
+  }
+  
 };
